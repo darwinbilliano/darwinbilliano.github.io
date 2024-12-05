f8676a4c369860c21544a4426a083cafecb134a0
Darwin Billiano <darwinbilliano@outlook.com>
2024-12-05 20:57:37 +0700

    feat: move about me to its own page and make nav visible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,19 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "About Me",
+  title: "Home",
 };
 
 export default function Home() {
   return (
-    <main>
-      <section id="about">
-        <h1>Hey there! I&apos;m Darwin Billiano</h1>
-        <br />
-        <p>
-          I&apos;m a hobbyist programmer since 2019. I began my programming
-          journey through a simple top-down shooter game I made using Java. From
-          there, I got into Unity and C# which has since become my favorite
-          programming language. I&apos;ve also expanded my skills to full-stack
-          web development and software development.
-        </p>
-        <br />
-        <p>
-          When I&apos;m not coding, I love playing video games, which often
-          inspires some of my projects. Games are also the reason I got into
-          programming in the first place, with Minecraft being the game that
-          inspired me the most and led me to learn Java.
-        </p>
-        <br />
-        <p>
-          When it comes to programming, one thing I&apos;m not a fan of is
-          dynamically typed and interpreted languages. I&apos;m looking at you,
-          Python and JavaScript!
-        </p>
-      </section>
-      <section id="skills">
-        <h1>Skills</h1>
-        <ul>
-          <li>C#</li>
-          <li>Avalonia UI</li>
-        </ul>
-        <ul>
-          <li>ASP.NET Core</li>
-          <li>Next.js (you&apos;re looking at it rn)</li>
-        </ul>
-        <ul>
-          <li>Godot</li>
-          <li>Unity</li>
-        </ul>
-        <ul>
-          <li>Git</li>
-          <li>Ubuntu</li>
-          <li>Arch Linux (I use arch btw)</li>
-        </ul>
+    <main className="justify-center">
+      <section className="align-center size-medium text-center">
+        <h1>Darwin Billiano</h1>
+        <p>Hi! I&apos;m a hobbyist programmer</p>
+        <Link href="/about" className="button">
+          Learn more
+        </Link>
       </section>
     </main>
   );
