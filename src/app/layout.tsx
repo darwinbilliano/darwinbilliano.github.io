eb3b7ec865ce78f41dac55ff3a6881e0627c977b
Darwin Billiano <darwinbilliano@proton.me>
2024-12-22 12:51:49 +0700

    build: add fontawesome package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Nunito_Sans } from "next/font/google";
 import type { ReactNode } from "react";
+
+// Disable fontawesome auto css injection
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const nunito = Nunito_Sans({
   subsets: ["latin"],
