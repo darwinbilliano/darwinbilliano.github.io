be475f44637cfd51d083baf49756fbb48713bd12
Darwin Billiano <darwinbilliano@proton.me>
2024-12-22 07:54:47 +0700

    refactor: typescript strict mode and postcss config file

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
+import type { HTMLProps } from "react";
 
-export default function HomeLayout({ children }) {
+export default function HomeLayout({ children }: HTMLProps<HTMLDivElement>) {
   return (
     <>
       <Header className="home" />
