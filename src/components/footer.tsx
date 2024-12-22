32c4ee8910de922e3393d55bd8c88ab4ce2b24c5
Darwin Billiano <darwinbilliano@proton.me>
2024-12-22 12:53:36 +0700

    feat: restyle footer and add social link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,15 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Footer() {
   return (
     <footer>
+      <div>
+        <a href="https://github.com/darwinbilliano">
+          <FontAwesomeIcon className="fa-xl" icon={faGithub} />
+        </a>
+      </div>
+      <br />
       <p>&copy; 2024 Darwin Billiano</p>
       <p>This website is under construction...</p>
     </footer>
