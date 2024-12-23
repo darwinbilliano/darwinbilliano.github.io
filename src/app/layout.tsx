e4b675e5d7e108b39ecd198d5f2b39af3d6f6d78
Darwin Billiano <darwinbilliano@outlook.com>
2024-12-23 12:53:08 +0700

    refactor: remove redundant fontawesome config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,6 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Nunito_Sans } from "next/font/google";
 import type { ReactNode } from "react";
 
-// Disable fontawesome auto css injection
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
-
 const nunito = Nunito_Sans({
   subsets: ["latin"],
 });
