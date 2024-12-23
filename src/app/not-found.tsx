5a8dc75f4e88f65f3fc721fcdbfe96569aad7729
Darwin Billiano <darwinbilliano@outlook.com>
2024-12-23 13:54:08 +0700

    feat: update about page skills section, sort tailwind classes, fix not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,17 +11,15 @@ export default function NotFound() {
   return (
     <>
       <Header />
-      <main className="flex flex-col">
-        <div className="flex flex-col">
-          <section className="w-full px-12 py-8 max-w-lg self-center text-center">
-            <h1 className="font-bold text-2xl">Not Found</h1>
-            <p>It looks like you&apos;re lost, here&apos;s the way back</p>
-            <br />
-            <Link className="btn" href="/">
-              Go Back
-            </Link>
-          </section>
-        </div>
+      <main className="flex flex-1 flex-col items-center">
+        <section className="max-w-lg px-12 py-8 text-center">
+          <h1 className="font-bold text-2xl">Not Found</h1>
+          <p>It looks like you&apos;re lost, here&apos;s the way back</p>
+          <br />
+          <Link className="btn" href="/">
+            Go Back
+          </Link>
+        </section>
       </main>
       <Footer />
     </>
