990a47d80b0c2728052da2d7bca9ed66c64415a5
Darwin Billiano <darwinbilliano@proton.me>
2025-01-02 16:58:45 +0700

    feat: remove link view from about page

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,7 +1,6 @@
 import type AboutModel from "@/models/about";
 import loader from "@/utils/loader";
 import type { Metadata } from "next";
-import Link from "next/link";
 
 function SkillsView({ model }: { model: AboutModel }) {
   return (
@@ -11,22 +10,6 @@ function SkillsView({ model }: { model: AboutModel }) {
           <div className="h-full p-4 bg-red border border-medium rounded">
             <p className="font-bold text-center">{skill}</p>
           </div>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function LinksView({ model }: { model: AboutModel }) {
-  return (
-    <ul className="flex max-sm:flex-col gap-4">
-      {model.links.map(({ href, label }) => (
-        <li className="flex-1" key={href}>
-          <Link href={href}>
-            <div className="p-4 bg-red hover:bg-red-light border border-medium rounded">
-              <p className="font-bold text-center">{label}</p>
-            </div>
-          </Link>
         </li>
       ))}
     </ul>
@@ -56,13 +39,6 @@ export default async function AboutPage() {
           <h1 className="font-bold text-2xl text-center">Skills</h1>
           <br />
           <SkillsView model={model} />
-        </section>
-      </div>
-      <div className="flex flex-col items-center">
-        <section className="w-full max-w-2xl">
-          <h1 className="font-bold text-2xl text-center">Go Deeper</h1>
-          <br />
-          <LinksView model={model} />
         </section>
       </div>
     </main>
