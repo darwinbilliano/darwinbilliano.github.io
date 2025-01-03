875f1ff69514c3327626543eee095a9ab73a3b0c
Darwin Billiano <darwinbilliano@proton.me>
2025-01-03 19:34:54 +0700

    feat: move social links to footer

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,12 @@
 import type HeaderModel from "@/models/header";
-import type SocialModel from "@/models/social";
 import yaml from "@/utils/content/yaml";
-import { findIconDefinition } from "@fortawesome/fontawesome-svg-core";
 import Nav from "./nav";
 
 export default async function Header({ className }: { className?: string }) {
   const model = await yaml.read<HeaderModel>("header.yaml");
-  const socialModel = await yaml
-    .read<SocialModel>("social.yaml")
-    .then(({ links }) => ({
-      links: links.map(({ href, icon }) => ({
-        href,
-        icon: findIconDefinition(icon),
-      })),
-    }));
-
   return (
     <header className={className}>
-      <Nav model={model} socialModel={socialModel} />
+      <Nav model={model} />
     </header>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import type NavModel from "@/models/header";
-import type SocialModel from "@/models/social";
+import type HeaderModel from "@/models/header";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
@@ -9,7 +8,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
-function NavLinks({ model }: { model: NavModel }) {
+function NavLinks({ model }: { model: HeaderModel }) {
   const path = usePathname();
   return (
     <ul className="flex max-lg:flex-col">
@@ -32,33 +31,9 @@ function NavLinks({ model }: { model: NavModel }) {
   );
 }
 
-function SocialLinks({ model }: { model: SocialModel }) {
-  return (
-    <ul className="flex items-center justify-center">
-      {model.links.map(({ href, icon }) => {
-        return (
-          <li className="flex" key={href}>
-            <Link className="p-3 lg:p-2" href={href}>
-              <FontAwesomeIcon className="fa-xl" icon={icon} />
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-function Links({
-  model,
-  socialModel,
-  toggle,
-}: {
-  model: NavModel;
-  socialModel: SocialModel;
-  toggle: boolean;
-}) {
+function Links({ model, toggle }: { model: HeaderModel; toggle: boolean }) {
   const className = classNames(
-    "flex max-lg:flex-col items-stretch lg:gap-8 w-full max-lg:py-4 max-lg:absolute left-0 top-14 bg-inherit border-inherit max-lg:border-b",
+    "flex max-lg:flex-col items-stretch w-full max-lg:py-4 max-lg:absolute left-0 top-14 bg-inherit border-inherit max-lg:border-b",
     {
       "max-lg:hidden": !toggle,
     }
@@ -66,18 +41,11 @@ function Links({
   return (
     <div className={className}>
       <NavLinks model={model} />
-      <SocialLinks model={socialModel} />
     </div>
   );
 }
 
-export default function Nav({
-  model,
-  socialModel,
-}: {
-  model: NavModel;
-  socialModel: SocialModel;
-}) {
+export default function Nav({ model }: { model: HeaderModel }) {
   const [state, setState] = useState(false);
 
   function toggle() {
@@ -94,12 +62,12 @@ export default function Nav({
   }, [state]);
 
   return (
-    <nav className="flex items-center justify-between h-14 px-12 lg:px-32 relative font-bold bg-inherit border-inherit">
+    <nav className="flex items-center justify-between h-14 px-12 lg:px-32 font-bold bg-inherit border-inherit">
       <h1 className="text-xl">
         <Link href="/">@billiano</Link>
       </h1>
       <div className="flex items-center bg-inherit border-inherit">
-        <Links model={model} socialModel={socialModel} toggle={state} />
+        <Links model={model} toggle={state} />
         <div className="lg:hidden">
           <button className="px-4 py-2" onClick={toggle}>
             <FontAwesomeIcon className="fa-xl" icon={faBars} />
