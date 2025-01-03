efaf41e1f9499859e08d170d459e88140f2e4c1b
Darwin Billiano <darwinbilliano@proton.me>
2025-01-03 19:53:16 +0700

    feat: update and refactor header

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,71 +8,71 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
-function NavLinks({ model }: { model: HeaderModel }) {
+function NavMenu({ model, state }: { model: HeaderModel; state: boolean }) {
   const path = usePathname();
-  return (
-    <ul className="flex max-lg:flex-col">
-      {model.links.map(({ href, label }) => {
-        const className = classNames(
-          "w-full px-12 lg:px-6 py-3 lg:py-2 hover:underline",
-          {
-            underline: path === href,
-          }
-        );
-        return (
-          <li className="flex" key={href}>
-            <Link className={className} href={href}>
-              {label}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-function Links({ model, toggle }: { model: HeaderModel; toggle: boolean }) {
   const className = classNames(
-    "flex max-lg:flex-col items-stretch w-full max-lg:py-4 max-lg:absolute left-0 top-14 bg-inherit border-inherit max-lg:border-b",
+    "max-lg:absolute left-0 right-0 top-14 max-lg:py-4 bg-inherit border-inherit max-lg:border-b",
     {
-      "max-lg:hidden": !toggle,
+      "max-lg:hidden": !state,
     }
   );
   return (
     <div className={className}>
-      <NavLinks model={model} />
+      <ul className="flex max-lg:flex-col">
+        {model.links.map(({ href, label }) => {
+          const className = classNames(
+            "px-12 lg:px-6 py-3 lg:py-2 hover:underline",
+            {
+              underline: path === href,
+            }
+          );
+          return (
+            <li className="flex flex-col" key={href}>
+              <Link className={className} href={href}>
+                {label}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function HamburgerMenu({
+  state,
+  setState,
+}: {
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const toggle = () => setState((state) => !state);
+
+  useEffect(() => {
+    const close = () => setState(false);
+    document.addEventListener("click", close);
+    return () => document.removeEventListener("click", close);
+  }, [state, setState]);
+
+  return (
+    <div className="lg:hidden">
+      <button className="px-2 py-3" onClick={toggle}>
+        <FontAwesomeIcon className="fa-xl" icon={faBars} />
+      </button>
     </div>
   );
 }
 
 export default function Nav({ model }: { model: HeaderModel }) {
   const [state, setState] = useState(false);
-
-  function toggle() {
-    setState((state) => !state);
-  }
-
-  function close() {
-    setState(false);
-  }
-
-  useEffect(() => {
-    document.addEventListener("click", close);
-    return () => document.removeEventListener("click", close);
-  }, [state]);
-
   return (
     <nav className="flex items-center justify-between h-14 px-12 lg:px-32 font-bold bg-inherit border-inherit">
       <h1 className="text-xl">
-        <Link href="/">@billiano</Link>
+        <Link href="/">@darwinbilliano</Link>
       </h1>
-      <div className="flex items-center bg-inherit border-inherit">
-        <Links model={model} toggle={state} />
-        <div className="lg:hidden">
-          <button className="px-4 py-2" onClick={toggle}>
-            <FontAwesomeIcon className="fa-xl" icon={faBars} />
-          </button>
-        </div>
+      <div className="flex items-center lg:gap-8 bg-inherit border-inherit">
+        <NavMenu model={model} state={state} />
+        <HamburgerMenu state={state} setState={setState} />
       </div>
     </nav>
   );
