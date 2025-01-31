824b6dbd2796204e5810285ea646db8cd8620668
Darwin Billiano <darwinbilliano@proton.me>
2025-01-31 18:40:31 +0700

    feat: footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-zinc-950 text-gray-300">{children}</body>
+      <body className="flex min-h-screen flex-col bg-zinc-950 text-gray-300">
+        <main className="flex-1">{children}</main>
+        <footer className="p-12 text-center">
+          &copy; 2025 Darwin Billiano
+        </footer>
+      </body>
     </html>
   );
 }
