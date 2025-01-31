30fc21073590db0fd363a5a31eda9b41778e9745
Darwin Billiano <darwinbilliano@proton.me>
2025-01-31 18:40:41 +0700

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "404",
+};
+
+export default function NotFoundPage() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <section className="p-12 text-center">
+        <h1 className="mb-2 font-display text-3xl">404</h1>
+        <p className="mb-6 font-display">
+          It looks like you&apos;re lost. Here is the way back!
+        </p>
+        <Link className="btn" href="/">
+          Go Back
+        </Link>
+      </section>
+    </div>
+  );
+}
