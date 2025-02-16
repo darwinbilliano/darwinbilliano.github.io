9d7f82e337e780a7ed1833854f3f289679278adc
Darwin Billiano <darwinbilliano@proton.me>
2025-02-16 07:59:11 +0700

    style: reduce margin in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFoundPage() {
   return (
     <div className="grid min-h-screen place-items-center">
       <section className="p-12 text-center">
-        <h1 className="mb-6 font-display text-5xl text-white md:text-7xl">
+        <h1 className="mb-2 font-display text-5xl text-white md:text-7xl">
           404
         </h1>
         <p className="mb-6 font-display text-xl">
