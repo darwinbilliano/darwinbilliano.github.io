968d4c084adc9d1a3e8257cba128b46a3876c79a
Darwin Billiano <darwinbilliano@proton.me>
2025-02-19 03:15:26 +0700

    feat: add try-catch to assets utils and refactor typings

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,9 +1,13 @@
-import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+import type { StaticImageData } from "next/dist/shared/lib/get-img-props";
 
 const assets = {
-  svg: async function (id: string) {
-    const asset = await import(`@/assets/${id}`);
-    return asset.default as StaticImport;
+  svg: async function (id: string): Promise<string | StaticImageData> {
+    try {
+      const asset = await import(`@/assets/${id}`);
+      return asset.default as StaticImageData;
+    } catch {
+      return "";
+    }
   },
 };
 
