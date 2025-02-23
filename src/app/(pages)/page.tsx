a308462781ac65f01c9c8263077150a900f9396c
Darwin Billiano <darwinbilliano@proton.me>
2025-02-23 19:06:39 +0700

    refactor: rename panel to card

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -15,10 +15,10 @@ export default function HomePage() {
             <h1 className="section-title md:text-7xl">Darwin Billiano</h1>
             <p className="section-description md:text-xl">Hobbyist Software and Full-stack Developer</p>
           </header>
-<div>
-          <a className="btn" href="#about">
-            Learn More
-          </a>
+          <div>
+            <a className="btn" href="#about">
+              Learn More
+            </a>
           </div>
         </section>
       </div>
@@ -30,21 +30,21 @@ export default function HomePage() {
               <p className="section-description">Here are some technologies I&apos;ve used in past projects:</p>
             </header>
             <div>
-            <ul className="grid grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] gap-4">
-              {skills.map(({ name, category, icon, style }) => (
-                <li key={name}>
-                  <div className="panel flex items-center gap-4 duration-200 hover:scale-105">
-                    <div className="rounded-md p-2" style={style}>
-                      <Image src={icon} alt={name} width={48} height={48} />
+              <ul className="grid grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] gap-4">
+                {skills.map(({ name, category, icon, style }) => (
+                  <li key={name}>
+                    <div className="card flex items-center gap-4 duration-200 hover:scale-105">
+                      <div className="rounded-md p-2" style={style}>
+                        <Image src={icon} alt={name} width={48} height={48} />
+                      </div>
+                      <div>
+                        <h2 className="text-white">{name}</h2>
+                        <p className="text-sm">{category}</p>
+                      </div>
                     </div>
-                    <div>
-                      <h2 className="text-white">{name}</h2>
-                      <p className="text-sm">{category}</p>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                  </li>
+                ))}
+              </ul>
             </div>
           </section>
         </div>
